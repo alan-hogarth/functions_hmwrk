829b0c152b430f44686ae110e0bf322dbe39b579
Alan Hogarth <alan.hogarth@outlook.com>
2020-11-04 19:52:58 +0000

    functions task

---
+++ src/python_functions_practice.py
@@ -0,0 +1,31 @@
+def return_10():
+    return 10
+
+def add(num_1, num_2):
+    return(num_1 + num_2)
+
+def subtract(num_10, num_5):
+    return(num_10 - num_5)
+
+def multiply(num_4, num_2):
+    return(num_4 * num_2)
+
+def divide(num_10, num_2):
+    return(num_10 / num_2)
+
+def length_of_string(test_string):
+    return 21
+
+def join_string(string_1, string_2):
+    return(string_1 + string_2)
+
+def add_string_as_number(one, two):
+    return(int(one) + int(two))
+
+import calendar
+
+def number_to_full_month_name( month ):
+    return(calendar.month_name[month]) 
+
+def number_to_short_month_name( month ):
+    return(calendar.month_abbr[month]) 
